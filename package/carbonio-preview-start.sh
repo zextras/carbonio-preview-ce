c71b9083df50762b0cc4e7340d5f7e7537f68722
M0Rf30 <morf3089@gmail.com>
2022-06-30 18:57:02 +0200

    chore: update the starting script

--- package/carbonio-preview-start.sh
+++ package/carbonio-preview-start.sh
@@ -17,5 +17,6 @@
 # cd could be avoided but we do it nonetheless
 
 export JAVA_HOME=/opt/zextras/common/lib/jvm/java
+PYTHONPATH="/opt/zextras/common/lib/python3.8/site-packages/:${PYTHONPATH}" \
 /opt/zextras/common/bin/gunicorn app.controller:app \
   --config /opt/zextras/common/lib/python3.8/site-packages/app/gunicorn.conf.py
