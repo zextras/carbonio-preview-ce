205d232cd3a1bc56ba0f2c16481e393d4b19a9c3
Daniele Giachetto <daniele.giachetto@zextras.com>
2022-07-20 17:25:09 +0200

    chore: bump version

--- app/controller.py
+++ app/controller.py
@@ -14,7 +14,7 @@ from app.core.routers import image, health, pdf, document
 
 app = FastAPI(
     title=service.NAME,
-    version="0.2.9",
+    version="0.2.10",
     description=service.DESCRIPTION,
 )
 
