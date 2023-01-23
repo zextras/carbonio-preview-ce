928d12b481919bbd19521f13d5a71117ba1c2c40
Daniele Giachetto <daniele.giachetto@zextras.com>
2023-01-23 12:09:07 +0100

    feat: remove extra headers from beginning of pdf, update versions, update pre-commit test run, update libraries

--- tests/core/services/document_manipulation/test_document_manipulation.py
+++ tests/core/services/document_manipulation/test_document_manipulation.py
@@ -4,6 +4,8 @@
 
 import io
 import sys
+import pytest
+from typing import List
 from unittest import IsolatedAsyncioTestCase
 from unittest.mock import MagicMock, patch
 
@@ -42,7 +44,11 @@ class TestPdfManipulation(IsolatedAsyncioTestCase):
         "app.core.services.document_manipulation.document_manipulation._parse_if_valid_pdf",
         return_value=None,
     )
-    def test_split_pdf_invalid_pdf(self, mock_parse_pdf):
+    @patch(
+        "app.core.services.document_manipulation.document_manipulation._get_sanitize_offset",
+        return_value=0,
+    )
+    def test_split_pdf_invalid_pdf(self, mock_sanitize_offset, mock_parse_pdf):
         # TODO(RakuJa): Remove this empty_pdf_buffer, mock write to buffer and check its input arguments
         empty_pdf_bytes: bytes = (
             b"%PDF-1.3\n%\xe2\xe3\xcf\xd3\n1 0 "
@@ -56,21 +62,155 @@ class TestPdfManipulation(IsolatedAsyncioTestCase):
         result = document_manipulation.split_pdf(io.BytesIO(), 0, 1)
         self.assertEqual(result.read(), empty_pdf_bytes)
         self.assertEqual(mock_parse_pdf.call_count, 1)
+        self.assertEqual(mock_sanitize_offset.call_count, 1)
 
     @patch(
         "app.core.services.document_manipulation.document_manipulation._parse_if_valid_pdf",
         return_value=True,
     )
-    def test_split_pdf_valid_pdf_no_pages_to_split(self, mock_parse_pdf):
+    @patch(
+        "app.core.services.document_manipulation.document_manipulation._get_sanitize_offset",
+        return_value=0,
+    )
+    def test_split_pdf_valid_pdf_no_pages_to_split(
+        self, mock_sanitize_offset, mock_parse_pdf
+    ):
         result = document_manipulation.split_pdf(io.BytesIO(), 1, 0)
         self.assertEqual(result.read(), b"")
         self.assertEqual(mock_parse_pdf.call_count, 1)
+        self.assertEqual(mock_sanitize_offset.call_count, 1)
 
     @patch(
         "app.core.services.document_manipulation.document_manipulation._parse_if_valid_pdf",
         return_value=encrypted_pdf_mock,
     )
-    def test_split_encrypted_pdf(self, mock_parse_pdf):
+    @patch(
+        "app.core.services.document_manipulation.document_manipulation._get_sanitize_offset",
+        return_value=0,
+    )
+    def test_split_encrypted_pdf(self, mock_sanitize_offset, mock_parse_pdf):
         result = document_manipulation.split_pdf(io.BytesIO(b"ciao"), 2, 5)
         self.assertEqual(result.read(), b"ciao")
         self.assertEqual(mock_parse_pdf.call_count, 1)
+        self.assertEqual(mock_sanitize_offset.call_count, 1)
+
+
+def test_given_pdf_without_extra_headers_sanitize_offset_should_return_0(expect):
+    # Given
+    buff_argument = io.BytesIO()
+    pdf_content: List[str] = ["%PDF-1.5", "pdf_content1", "pdf_content2"]
+    expect(document_manipulation, times=1)._read_buffer_line_by_line(
+        buff_argument
+    ).thenReturn(pdf_content)
+
+    # When
+    result = document_manipulation._get_sanitize_offset(buff_argument)
+
+    # Then
+    assert result == 0
+
+
+def test_given_pdf_with_one_extra_headers_sanitize_offset_should_return_len_first_string(
+    expect,
+):
+    # Given
+    buff_argument = io.BytesIO()
+    pdf_content: List[str] = ["extra-header" "%PDF-1.5", "pdf_content1"]
+    expect(document_manipulation, times=1)._read_buffer_line_by_line(
+        buff_argument
+    ).thenReturn(pdf_content)
+
+    # When
+    result = document_manipulation._get_sanitize_offset(buff_argument)
+
+    # Then
+    assert result == 12
+
+
+@pytest.mark.parametrize(
+    "in_out_tuple",
+    [
+        (["x%PDF-1.5"], 1),
+        (["xxxx%PDF-1.5"], 4),
+        (["xxxxxxxxxxx%PDF-1.5"], 11),
+        (["x%PDF-1.5", "pdf-content"], 1),
+        (["pdf-content", "x%PDF-1.5"], 12),
+        (["xxxx%PDF-1.5", "pdf-content"], 4),
+        (["pdf-content", "xxxx%PDF-1.5"], 15),
+        (["xxxxxxxxxxx%PDF-1.5", "pdf-content"], 11),
+        (["pdf-content", "xxxxxxxxxxx%PDF-1.5"], 22),
+    ],
+)
+def test_given_pdf_with_extra_headers_sanitize_offset_should_return_correct_size(
+    expect, in_out_tuple
+):
+    # Given
+    buff_argument = io.BytesIO()
+    expect(document_manipulation, times=1)._read_buffer_line_by_line(
+        buff_argument
+    ).thenReturn(in_out_tuple[0])
+
+    # When
+    result = document_manipulation._get_sanitize_offset(buff_argument)
+
+    # Then
+    assert result is in_out_tuple[1]
+
+
+def test_given_pdf_without_correct_header_and_multiple_lines_offset_should_return_str_size(
+    expect,
+):
+    # Given
+    buff_argument = io.BytesIO()
+    str_to_search = ["zextras", "-", "test-rakuja"]
+    expect(document_manipulation, times=1)._read_buffer_line_by_line(
+        buff_argument
+    ).thenReturn(str_to_search)
+
+    # When
+    result = document_manipulation._get_sanitize_offset(buff_argument)
+
+    # Then
+    assert result is sum(len(x) for x in str_to_search)
+
+
+def test_given_pdf_without_correct_header_and_single_line_offset_should_return_str_size(
+    expect,
+):
+    # Given
+    buff_argument = io.BytesIO()
+    str_to_search = "zextras-test-rakuja"
+    expect(document_manipulation, times=1)._read_buffer_line_by_line(
+        buff_argument
+    ).thenReturn(str_to_search)
+
+    # When
+    result = document_manipulation._get_sanitize_offset(buff_argument)
+
+    # Then
+    assert result is len(str_to_search)
+
+
+def test_given_empty_buffer_read_by_line_should_return_nothing():
+    # Given
+    buff_argument = io.BytesIO()
+
+    # When
+    result = document_manipulation._read_buffer_line_by_line(buff_argument)
+
+    # Then
+    with pytest.raises(StopIteration) as e_info:
+        next(result)
+    assert e_info.value.value is None
+
+
+def test_given_buffer_with_one_line_read_by_line_should_return_one_line():
+    # Given
+    buff_argument = io.BytesIO(b"first-line")
+
+    # When
+    result = document_manipulation._read_buffer_line_by_line(buff_argument)
+
+    # Then
+    for line in result:
+        assert b"first-line" == line
