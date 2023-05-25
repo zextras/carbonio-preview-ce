4a6187c0d6aa1d72328f553fb592cbea4a86eff3
RakuJa <daniele.giachetto@zextras.com>
2023-05-25 17:06:07 +0200

    chore(release): 0.3.2 (#37)

--- app/controller.py
+++ app/controller.py
@@ -12,7 +12,7 @@ from app.core.routers import image, health, pdf, document
 
 app = FastAPI(
     title=service.NAME,
-    version="0.3.1-2",
+    version="0.3.2-1",
     description=service.DESCRIPTION,
 )
 
